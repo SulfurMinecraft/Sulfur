cdaa23bfd074b8027b8fd22ea23d9dd794d4c5c0
jgj52 <email@jgj52.hu>
2026-04-07 19:58:48 +0200

    fix small bugs

--- src/main/java/hu/jgj52/Sulfur/Commands/PluginsCommand.java
+++ src/main/java/hu/jgj52/Sulfur/Commands/PluginsCommand.java
@@ -6,14 +6,17 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
-import net.minestom.server.command.builder.Command;
+import net.kyori.adventure.text.minimessage.MiniMessage;import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;import net.minestom.server.command.builder.Command;
 
 public class PluginsCommand extends Command {
     public PluginsCommand() {
         super("plugins", "pl");
 
         setDefaultExecutor((sender, _) -> {
-            Component plugins = Component.empty();
+            Component plugins = MiniMessage.miniMessage().deserialize(
+                    Sulfur.conf.get("messages").getAsJsonObject().get("plugin").getAsString(),
+                    Placeholder.parsed("count", String.valueOf(Sulfur.loadedPlugins.size()))
+            );
             boolean first = true;
             for (LoadedPlugin plugin : Sulfur.loadedPlugins.values()) {
                 Component p = Component.text(first ? "" : ", ").color(NamedTextColor.WHITE).append(
